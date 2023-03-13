7a6597bcef6c779de593017e9ba0e31745690ef6
suprabash <suprabash@esignature.com.np>
2023-03-13 12:23:09 +0545

    uncaught exception error handller done

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
+
+// Unhandled Exception  :  USE IN PRODUCTION NOT DEV
+process.on("uncaughtException", (err) => {
+  console.log("uncaught exception is :", err.message);
+  console.log("Shutting down server due to uncaught expeptions");
+  process.exit(1);
+});
+
 const server = app.listen(process.env.PORT, () => {
   console.log(`server starting on : localhost://${process.env.PORT}`);
 });
 
-// Unhandled Promise Rejection:-
-process.on("unhandledRejection", (err) => {
-  console.log("unhandlled error is", err.message);
-  console.log("Shutting down server due to unhandled Promise Rejection");
-  server.close(() => {
-    process.exit(1);
-  });
-});
+// Unhandled Promise Rejection :  USE IN PRODUCTION NOT DEV
+
+// process.on("unhandledRejection", (err) => {
+//   console.log("unhandlled error is:", err.message);
+//   console.log("Shutting down server due to unhandled Promise Rejection");
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });
